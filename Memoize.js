3224921546f15d8be985d689d90ad5a80f496f2d
harshlocked7 <harshlocked7@gmail.com>
2024-02-13 22:06:43 +0530

    Memoize

---
+++ Memoize.js
@@ -0,0 +1,67 @@
+// Given a function fn, return a memoized version of that function.
+
+// A memoized function is a function that will never be called twice with the same inputs. Instead it will return a cached value.
+
+// You can assume there are 3 possible input functions: sum, fib, and factorial.
+
+// sum accepts two integers a and b and returns a + b.
+// fib accepts a single integer n and returns 1 if n <= 1 or fib(n - 1) + fib(n - 2) otherwise.
+// factorial accepts a single integer n and returns 1 if n <= 1 or factorial(n - 1) * n otherwise.
+ 
+function memoize(fn) {
+    const cache = new Map();
+    const callCount = { count: 0 };
+
+    const memoizedFn = (...args) => {
+        const key = JSON.stringify(args);
+
+        if (cache.has(key)) {
+            return cache.get(key);
+        } else {
+            const result = fn(...args);
+            cache.set(key, result);
+            callCount.count++;
+            return result;
+        }
+    };
+
+    memoizedFn.getCallCount = () => callCount.count;
+
+    return memoizedFn;
+}
+
+// Example usage:
+const sum = (a, b) => a + b;
+const factorial = (n) => (n <= 1) ? 1 : (n * factorial(n - 1));
+const fib = (n) => (n <= 1) ? 1 : (fib(n - 1) + fib(n - 2));
+
+const memoizedSum = memoize(sum);
+const memoizedFactorial = memoize(factorial);
+const memoizedFib = memoize(fib);
+
+const fnName = "sum";
+const actions = ["call", "call", "getCallCount", "call", "getCallCount"];
+const values = [[2, 2], [2, 2], [], [1, 2], []];
+
+const output = [];
+let currentMemoizedFn;
+
+for (let i = 0; i < actions.length; i++) {
+    const action = actions[i];
+
+    switch (action) {
+        case "call":
+            currentMemoizedFn = (fnName === "sum") ? memoizedSum :
+                                (fnName === "factorial") ? memoizedFactorial :
+                                (fnName === "fib") ? memoizedFib : null;
+
+            output.push(currentMemoizedFn(...values[i]));
+            break;
+
+        case "getCallCount":
+            output.push(currentMemoizedFn.getCallCount());
+            break;
+    }
+}
+
+console.log(output); // Output: [4, 4, 1, 3, 2]
